f72220fd957dbfd836dc274fc114207f87372ef4
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2025-08-28 21:32:52 -0300

    🚧 improvements
    improved readability
    now, the name and value are displayed when purchasing them

--- cookieClicker.py
+++ cookieClicker.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 service = Service(executable_path = "chromedriver.exe")
@@ -18,11 +19,54 @@ product_prefix = "product"
 product_name_id_prefix = "productName"
 upgrade_id_prefix = "upgrade"
 
+def clickOnSuperCookie():
+    cookie = driver.find_element(By.ID, cookie_id)
+    cookie.click()
+
+def getCookiesCount():
+    cookies_count = driver.find_element(By.ID, cookiesCounter_id).text.split(" ")[0].replace(","," ")
+    cookies_count = int(cookies_count)
+    return cookies_count
+
+def checkUpgradeAvailability():
+        try:
+            upgrade_available = driver.find_element(By.CSS_SELECTOR, "#upgrade0.crate.upgrade.enabled")
+            if upgrade_available:
+                upgrade = driver.find_element(By.CSS_SELECTOR, ".upgrade")
+                ActionChains(driver).move_to_element(upgrade).perform()
+
+                wait = WebDriverWait(driver, 10)
+
+                upgrade_name = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#tooltip .name"))).text
+                upgrade_price = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#tooltip .price"))).text
+
+                upgrade_available.click()
+                print(f"Comprou o upgrade '{upgrade_name}' por {upgrade_price} cookies!")
+        except Exception:
+            pass
+
+def checkProductAvailability():
+    searchProductsAvailable = driver.find_elements(By.CLASS_NAME, "product.unlocked.enabled")
+    productsAvailable = len(searchProductsAvailable)
+
+    cookies_count = getCookiesCount()
+
+    for i in range(productsAvailable, -1, -1):
+            product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.split(" ")[0].replace(","," ")
+            product_price = int(product_price)
+
+            if cookies_count >= product_price:
+                product = driver.find_element(By.ID, product_prefix + str(i))
+                product.click()
+                product_name = driver.find_element(By.ID, product_name_id_prefix + str(i)).text
+                print(f"Comprou '{product_name}' por {product_price} cookies!")
+                break
+
+
 WebDriverWait(driver, 30).until(
     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Português')]"))
 )
 language = driver.find_element(By.XPATH, "//*[contains(text(), 'Português')]")
-time.sleep(1)
 language.click()
 
 WebDriverWait(driver, 30).until(
@@ -33,31 +77,9 @@ time.sleep(1)
 
 while True:
     try:
-        cookie = driver.find_element(By.ID, cookie_id)
-        cookie.click()
-
-        cookies_count = driver.find_element(By.ID, cookiesCounter_id).text.split(" ")[0].replace(","," ")
-        cookies_count = int(cookies_count)
-
-        try:
-            upgrade_available = driver.find_element(By.CSS_SELECTOR, "#upgrade0.crate.upgrade.enabled")
-            if upgrade_available:
-                upgrade_available.click()
-                print("Comprou um upgrade disponível!")
-        except Exception:
-            pass
-
-        for i in range(50):
-            product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.split(" ")[0].replace(","," ")
-            product_price = int(product_price)
-
-            if cookies_count >= product_price:
-                product = driver.find_element(By.ID, product_prefix + str(i))
-                product.click()
-                product_name = driver.find_element(By.ID, product_name_id_prefix + str(i)).text
-                print(f"Comprou {product_name} por {product_price} cookies!")
-                print(f"Total de Cookies atualmente: {cookies_count - product_price}")
-                break
+        clickOnSuperCookie()
+        checkUpgradeAvailability()
+        checkProductAvailability()
 
     except Exception as e:
         continue
